b23bd7fd65d71074330d5ddfcfdc45b2035f763b
Erpica <erpica@gmail.com>
2026-04-11 08:28:08 +0200

    Recien clonado en  pc Irene

--- Intermediate/07_regular_expresions.py
+++ Intermediate/07_regular_expresions.py
@@ -1,13 +1,13 @@
 import re
 
-my_string = "Esta es la lección número 7: Expresiones regulares"
+my_string = "Esta es la lección número 7.\nLección llamada Expresiones regulares"
 my_other_string = "Esta no es la lección número 7: Expresiones regulares"
 
 print(re.match("Esta es la lección", my_string))
 print(re.match("Esta es la lección", my_other_string))
 print(re.match("Expresiones regulares", my_string))
 
-match = re.match("Esta es la lección", my_string, re.I)
+match = re.match("Esta es la lección", my_string, re.I) # re.I -> IgnoreCase
 print(match)
 start, end = match.span() 
 print(my_string[start:end])
@@ -19,3 +19,20 @@ if match is not None:
     print(match)
     start, end = match.span()
     print (my_other_string[start:end])
+
+# search # Solo encuentra la primera ocurrencia
+search = re.search("lección", my_string, re.I)
+start, end = search.span() 
+print(my_string[start:end])
+
+# findall # Encuentra todas las veces que aparece 
+findall = re.findall("lección", my_string, re.I)
+print(findall)
+
+# split
+print(re.split("\n", my_string))
+
+# sub
+print(re.sub("Expresiones regulares", "RegEx", my_string)) # Aquí esto no hace nada: re.I
+print(re.sub("Lección|lección", "LECCIÓN", my_string))
+print(re.sub("[L|l]ección|lección", "LECCIÓN", my_string))
